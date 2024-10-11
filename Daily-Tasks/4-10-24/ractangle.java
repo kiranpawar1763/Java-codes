71f0d68282a53dfd0ee13ce243656e458fc2094f
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-11 23:00:31 +0530

    Finding Ractangle size Using  cmd

---
+++ Daily-Tasks/4-10-24/ractangle.java
@@ -0,0 +1,13 @@
+//Ractangal  of the two number number take must be command line argument
+ public class ractangle
+ {
+	public static void main(String []args)
+	 {
+		int lenghth= Integer.parseInt(args[0]);
+		int breadth = Integer.parseInt(args[1]);
+		int Area = lenghth * breadth ;
+		System .out.println("Area of the Ractangle="+Area);
+
+	 }
+
+ }
