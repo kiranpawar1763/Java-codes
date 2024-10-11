b67cbd41c1f84528b3e2ec7817dd79fc52ebc27a
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-11 22:56:14 +0530

    Finding cube of number using cmd

---
+++ Daily-Tasks/4-10-24/cube.java
@@ -0,0 +1,13 @@
+//cube of the two number number take must be command line argument
+package com.classes;
+ public class cube
+ {
+	public static void main(String []args)
+	 {
+		int a= Integer.parseInt(args[0]);
+		int cube = a * a *a;
+		System .out.println("cube="+cube);
+
+	 }
+
+ }
