b08630f875cc66ecd93e7343e2606afdc67bf3a0
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-11 22:44:23 +0530

    Addition of 2 numbers using command line argument

---
+++ Daily-Tasks/3-10-24---basics/Addition.java
@@ -0,0 +1,21 @@
+package com.classes;
+
+public class Addition
+{
+    public static void main(String[] args)
+     {
+        
+      int num1 = Integer.parseInt(args[0]);  
+      int  num2 = Integer.parseInt(args[1]); 
+	
+      int sum = num1 + num2;
+            
+      //System.out.println("Addition of "+num1+" and "+num2+" is "+sum);
+
+	System.out.println("Number 1 : "+num1);
+	System.out.println(num1+num2);
+	System.out.println("Number 1 : "+num1+num2);
+
+
+    }
+}
