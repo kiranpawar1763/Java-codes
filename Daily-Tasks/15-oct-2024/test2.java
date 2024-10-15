73b9b8c0b16beb5d94bc419541af5b4ea2815bc6
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-15 11:42:35 +0530

    MCQ Question

---
+++ Daily-Tasks/15-oct-2024/test2.java
@@ -0,0 +1,19 @@
+package com.classes;
+public class test2
+{
+public static void main(String[] args)
+{
+	int a = 20, b=10;
+
+   if((a<b) && (++b < 20))
+	{
+	   System.out.println("Logical AND ");
+	}
+	else
+	{
+		System.out.println("b value is : "+b);
+	}
+
+}
+
+}
