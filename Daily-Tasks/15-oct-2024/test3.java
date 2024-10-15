a559c4b11841399b5626dfd1693127bed8998c23
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-15 11:47:56 +0530

    MCQ practice Question

---
+++ Daily-Tasks/15-oct-2024/test3.java
@@ -0,0 +1,18 @@
+package com.classes;
+public class test3 
+{
+    public static void main(String[] args)
+{
+	int a = 20, b=10;
+
+        if((a<b) & (++b < 20))
+	{
+	   System.out.println("Boolean AND ");
+	}
+	else
+	{
+		System.out.println("b value is : "+b);
+	}
+}
+    
+}
