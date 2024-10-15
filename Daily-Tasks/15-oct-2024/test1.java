6c9d4c40897e1bd1a3616081decf43f7dbd9cd06
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-15 11:35:25 +0530

    Mcq Question

---
+++ Daily-Tasks/15-oct-2024/test1.java
@@ -0,0 +1,15 @@
+package com.classes;
+class test1
+{
+    public static void main(String[]args)
+    
+        {
+            int i = 0;
+     
+             int j = i++ + i;
+     
+             System.out.printf("%d\n", j);
+     
+         }
+    
+}
