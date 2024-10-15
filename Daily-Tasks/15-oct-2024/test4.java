5d023d17c890ec8256c8b8451b7b83ed621c751f
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-15 11:51:03 +0530

    MCQ practice Quetion

---
+++ Daily-Tasks/15-oct-2024/test4.java
@@ -0,0 +1,15 @@
+package com.classes;
+public class test4 
+{
+    public static void main(String[] args)
+{
+	      int z = 5;
+		  if(++z > 5 || ++z > 6)   //Logical OR
+		  {
+			  z++;
+		  }
+		  System.out.println(z);
+
+}
+    
+}
