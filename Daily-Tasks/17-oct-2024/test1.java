43fc7834b675fd4884d2484d00d254a4e9e830c7
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-17 12:26:37 +0530

    MCQ practice Question

--- Daily-Tasks/17-oct-2024/test1.java
+++ Daily-Tasks/17-oct-2024/test1.java
@@ -1,4 +1,4 @@
-package com.classes;
+
 class test1
 {
     public static void main(String[]args)
---
+++ Daily-Tasks/17-oct-2024/test2.java
@@ -0,0 +1,16 @@
+package com.classes;
+
+public class test2
+{
+    public static void main(String [ ] args)
+   {
+     int a=1;
+     switch(a)
+     {
+       default:
+       System.out.println("You choose correct option");
+     }
+   }
+   
+    
+}
