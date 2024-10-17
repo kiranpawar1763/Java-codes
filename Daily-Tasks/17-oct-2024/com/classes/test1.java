fdcf1fc6e7a22a569f64b88a4dcae18e9264b9f3
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-17 12:13:56 +0530

    MCQ Practice Quetion

---
+++ Daily-Tasks/17-oct-2024/com/classes/test1.java
@@ -0,0 +1,12 @@
+package com.classes;
+class test1
+{
+    public static void main(String[]args)
+    {
+        for(;false;)
+        System.out.println("Hello world!");
+    }
+
+}
+
+// o/p =error
