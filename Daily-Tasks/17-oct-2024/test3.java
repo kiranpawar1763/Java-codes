293c0fd81851289a55ced1e01fcc831b1e6b628d
Kiran Pawar <kiranpawar7662@gmail.com>
2024-10-17 12:41:30 +0530

    Switch case Mcq Practice Quetion

---
+++ Daily-Tasks/17-oct-2024/test3.java
@@ -0,0 +1,26 @@
+
+package com.classes;
+public class test3
+{
+    public static void main(String[]args)
+    {
+        for(int i=0;i<=3;i++)
+        {
+            switch(i)
+            {
+                case 0:
+                System.out.println("i is 0");
+                break;
+                case 1:
+                System.out.println("i is 1");
+                break;
+                case 2:
+                System.out.println("i is 2");
+                break;
+                default:
+                System.out.println("i is Greater than 2");
+            }
+        }
+    }
+    
+}
