6cd9f3576491603f7982818a8e8ed26dfbcc077c
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-10 17:40:26 +0530

    operators

---
+++ practice/main.java
@@ -0,0 +1,11 @@
+public class main
+{
+    public static void main(String[]args)
+    {
+       //operators
+       int a = 1;
+       int b = 2;
+       int mul = a*b;
+       System.out.println(mul);
+    }
+}
