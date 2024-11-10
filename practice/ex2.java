acd6942f1e0f4d48a2c1853103039f5df8381550
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-10 17:53:43 +0530

    take a input from user

---
+++ practice/ex2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex2 
+{
+    public static void main(String[]args)
+    {
+        //How to take input from user
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Age");
+        int age =sc. nextInt();
+        System.out.println("Your age is"+age);
+
+    }
+    
+}
