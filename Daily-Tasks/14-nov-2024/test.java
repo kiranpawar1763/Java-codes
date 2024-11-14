c68e6c7b04f6b9c4ce36c4b92d2869e3562073ce
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-14 12:41:35 +0530

    senerio base program -Animal has Fur

---
+++ Daily-Tasks/14-nov-2024/test.java
@@ -0,0 +1,69 @@
+class Animal
+{
+    private String name;
+    
+     
+    public Animal(String name) 
+    {
+        this.name = name;
+         
+
+    }
+
+    public void displayInfo() 
+    {
+        System.out.println("Animal name: " + name);
+        
+         
+    }
+    
+}
+
+
+class Mammal extends Animal
+{
+    private boolean  hasFur;
+    public Mammal(String name, boolean hasFur) 
+    {
+        super(name); 
+        this.hasFur = hasFur;
+    }
+    
+    @Override
+    public void displayInfo()
+    {
+        System.out.println("mammal details");
+        super.displayInfo();
+        System.out.println("hasFur"+hasFur);
+    }
+
+}
+class Dog extends Mammal
+{
+    private String breed;
+
+    public Dog(String name, boolean hasFur, String breed)
+     {
+        super(name, hasFur); 
+        this.breed = breed;
+    }
+
+    @Override
+    public void displayInfo()
+    {
+        System.out.println("breed details");
+        super.displayInfo();
+        System.out.println("brred type = "+breed);
+    }
+
+}
+
+
+public class test
+{
+    public static void main(String[] args)
+    {
+         Dog myDog = new Dog("Buddy", true, "Golden Retriever");
+          myDog.displayInfo();
+    }
+}
