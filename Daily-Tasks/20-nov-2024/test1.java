8ec7b0f0367626c213645ffdac493c44c12de2ae
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-20 12:23:19 +0530

    mcq output Question

---
+++ Daily-Tasks/20-nov-2024/test1.java
@@ -0,0 +1,21 @@
+public class test1 
+{
+	 public static void main(String[] args) 
+	 {
+		         int number = 1;
+		         String letters = "abc"; 
+		         number = number(number); 
+		         letters(letters);   
+		         System.out.println(number + letters);    
+		      }
+		      public static int number(int number) 
+		      {
+		        number++;
+		        return number;
+		     }
+		    public static String letters(String letters)//abc 
+		    {
+		        letters += "d";
+		        return letters;
+		     }
+}
