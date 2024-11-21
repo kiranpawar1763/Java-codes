11bdbce332dee43acf96506481cf6b5fc1f37bcf
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-21 12:07:30 +0530

    MCQ Question

---
+++ Daily-Tasks/21-NOV-2024/test1.java
@@ -0,0 +1,14 @@
+package com.classs;
+class Test1 
+{
+    public static void main (String[] args) 
+    {
+       int x=20;
+       System.out.println(x);
+   }
+  static 
+  {
+       int x = 10;
+       System.out.println(x+" ");
+ }
+}
