501fae6c0aa8f3e973b62ef164e0a9a100559094
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-21 12:27:42 +0530

    test4  mcq Quetion

---
+++ Daily-Tasks/21-NOV-2024/test4.java
@@ -0,0 +1,13 @@
+public class test4 
+{
+    
+    //static  int x =10; right position to declare static varible
+		public static void main(String[] args) 
+        {
+			System.out.println(x);
+		}
+	}
+    
+   // output = Error
+    //becuase  static keyword cannot be used to declare a variable inside a method.
+
