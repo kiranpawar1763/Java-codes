3dfefb7348bba2d2b7b08b4828c6b7d2c15898d7
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-21 12:19:09 +0530

    test3  mcq Quetion

---
+++ Daily-Tasks/21-NOV-2024/test2.java
@@ -0,0 +1,11 @@
+class test2
+ {
+	static 
+    {
+		System.out.println("Test2 class Static Block");
+}
+public static  void main(String[] args) 
+{
+	System.out.println("Test2 class main method");
+} 
+} 
---
+++ Daily-Tasks/21-NOV-2024/test3.java
@@ -0,0 +1,11 @@
+public class test3
+ {
+    public static  void  main(String[] args) 
+    {
+        int a;
+        System.out.println("A : "+a);
+   } 
+}
+output=Error
+    
+
