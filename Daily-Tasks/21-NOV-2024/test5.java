43a449cda7a191368e89bf9ddaff6de37a01cb87
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-21 12:30:15 +0530

    test 5 MCQ question

---
+++ Daily-Tasks/21-NOV-2024/test5.java
@@ -0,0 +1,13 @@
+package com.classes;
+public class test5 
+{
+    static int x = 10;
+    void m1() 
+    {
+        System.out.println("m1 X : "+x);
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println("main X : "+x);			
+    }
+}
