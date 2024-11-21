9a0207a7948d00c5f2d86c5b6cd13f14e4479a55
Kiran Pawar <kiranpawar7662@gmail.com>
2024-11-21 12:53:13 +0530

    How many ways to call static method

---
+++ Daily-Tasks/21-NOV-2024/test7.java
@@ -0,0 +1,28 @@
+public class test7 
+{
+    //static variable a
+		static int a = 20;
+		public static void main(String[] args) {
+			//write code here to access a variable with diff ways
+		}
+    
+}
+/*class Test7 {
+    // Static variable
+    static int a = 20; // Static variable belongs to the class, not instances
+
+    public static void main(String[] args) {
+        // Accessing the static variable in different ways
+
+       ****** // 1. Access directly by the variable name
+        System.out.println("Accessing directly: " + a);
+
+       ****** // 2. Access using the class name
+        System.out.println("Accessing using the class name: " + Test7.a);
+
+        ***** // 3. Access using an object of the class
+        Test7 obj = new Test7(); // Creating an object of Test7
+        System.out.println("Accessing using an object: " + obj.a);
+    }
+}
+*/
