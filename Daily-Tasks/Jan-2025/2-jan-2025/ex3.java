c509bf131914d6898a095e4448bb5963d42b9a6b
Kiran Pawar <kiranpawar7662@gmail.com>
2025-01-02 11:43:07 +0530

    ex-3 try catch

---
+++ Daily-Tasks/Jan-2025/2-jan-2025/ex3.java
@@ -0,0 +1,13 @@
+public class ex3 {
+	public static void main (String[] args) {
+		int a=20;
+		for(int i=0;i<a;i++)
+		   try
+		   {
+		     System.out.println(a/i);  
+		   }
+	}
+}
+
+//op-Error 
+//becuase- try catch we can't Declear without catch
