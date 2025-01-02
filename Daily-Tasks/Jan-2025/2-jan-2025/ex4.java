1000284bc425130bc7a1098cb7100f6147258bd1
Kiran Pawar <kiranpawar7662@gmail.com>
2025-01-02 11:50:36 +0530

    ex4-ExceptionHandling

---
+++ Daily-Tasks/Jan-2025/2-jan-2025/ex4.java
@@ -0,0 +1,25 @@
+class Age extends Exception
+{
+	Age(String str) {
+		super(str);
+	}
+}
+public class ex4
+{
+	static void nit(int age) throws Age
+	{
+		if(age<18)
+			throw new Age("Not for Vote");
+		else 
+		{
+			System.out.println("Vote");
+		}
+	}
+	public static void main(String[] args) {
+		try {
+			nit(15);
+		} catch (Age e) {
+			System.out.println("Exception "+e);
+		}		
+	}
+}
